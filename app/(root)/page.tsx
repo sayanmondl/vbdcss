be909a9bdc03c1cd09c9405a69bb8d4349a52d66
sayan <sayanmondal5869@gmail.com>
2025-03-20 20:03:56 +0530

    Update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import AnnouncementCard from "../components/AnnouncementCard";
+import Link from "next/link";
 import Button, { ButtonWhite } from "../components/Button";
-import EventCard from "../components/EventCard";
 import Load3LatestAnnouncements from "../components/Load3LatestAnnouncements";
 import Load3LatestEvents from "../components/Load3LatestEvents";
-import SportEventCard from "../components/SportEventCard";
+import LoadLatestSportEvents from "../components/LoadLatestSportEvents";
 
 export default function Home() {
   return (
@@ -15,11 +14,11 @@ export default function Home() {
             alt="home-banner"
             className="w-full h-[450px] object-cover"
           />
-          <h1 className="font-teko absolute text-8xl text-white w-1/2 text-center">
-            Innovating the Future, One Line of Code at a Time
+          <h1 className="font-teko absolute px-4 text-2xl xxs:text-4xl lg:text-6xl xl:text-7xl text-white xs:w-[350px] md:w-[500px] sm:w-[400px] lg:w-[600px] xl:w-[700px] xll:w-[900px] text-center">
+            Welcome to The Official website of Department of Computer and System Sciences.
           </h1>
         </div>
-        <div className="flex mt-10 gap-20 justify-center">
+        <div className="flex flex-col sm:flex-row mt-10 gap-3 sm:gap-20 justify-center items-center">
           <Button text="Explore Clubs" />
           <ButtonWhite text="View Announcements" />
         </div>
@@ -34,15 +33,14 @@ export default function Home() {
         <h1 className="font-teko text-3xl font-medium">LATEST ANNOUNCEMENTS</h1>
         <div className="h-0.5 bg-blue-dark w-3/4 mt-4"></div>
         <Load3LatestAnnouncements />
-        <Button text="View All Announcements" />
+        <Link href="/announcements">
+          <Button text="View All Announcements" />
+        </Link>
       </div>
       <div className="mt-20 flex flex-col items-center">
         <h1 className="font-teko text-3xl font-medium">LATEST SPORTS EVENTS</h1>
         <div className="h-0.5 bg-blue-dark w-3/4 mt-4"></div>
-        <div className="flex gap-10 my-16">
-          <SportEventCard />
-          <SportEventCard />
-        </div>
+        <LoadLatestSportEvents />
         <Button text="View All Sports Events" />
       </div>
       <div className="h-96"></div>
