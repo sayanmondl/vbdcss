f4fdddb47b56a9490519ff1a4c1ac929f8d91114
sayan <sayanmondal5869@gmail.com>
2025-03-31 22:08:34 +0530

    Create page to upload resources

---
+++ app/profile/uploadres/page.tsx
@@ -0,0 +1,10 @@
+import ResourceUploadForm from '@/app/components/ResourceUploadForm'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <ResourceUploadForm />
+  )
+}
+
+export default Page
