5e30d4b48aa6f7da43a0066b5b5c56ac1fb43d90
sayan <sayanmondal5869@gmail.com>
2025-05-02 20:19:47 +0530

    Add type definitions

---
+++ types.d.ts
@@ -0,0 +1,42 @@
+interface User {
+  id: string;
+  email: string;
+  emailVerified?: Date | null;
+  name: string;
+  image?: string | null;
+  role: "student" | "scholar" | "prof" | "admin" | string;
+  year?: number | null;
+  active: boolean;
+  about?: string | null;
+  links?: string[] | null;
+  goodIn?: string[] | null;
+}
+
+interface Announcement {
+  id: number;
+  title: string;
+  info: string;
+  attachment?: string | null;
+  isImportant?: boolean | null;
+  publish: Date;
+}
+
+export interface Event {
+  id: number;
+  title: string;
+  description: string;
+  location?: string | null;
+  date: Date | string;
+  coverUrl?: string | null;
+  archive?: string | null;
+}
+
+interface Tournament {
+  id: number;
+  name: string;
+  sport: string;
+  description?: string | null;
+  startDate: Date | string;
+  endDate: Date | string;
+  location?: string | null;
+}
