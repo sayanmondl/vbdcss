35341d3767c4891eacb41723839b6c59f0963c26
sayan <sayanmondal5869@gmail.com>
2025-06-07 20:43:54 +0530

    Replace dummy data with actual data from database

--- app/(root)/announcements/[announcementId]/page.tsx
+++ app/(root)/announcements/[announcementId]/page.tsx
@@ -1,19 +1,12 @@
 import Button from "@/app/components/Button";
 import { auth } from "@/auth";
 import { Badge } from "@/components/ui/badge";
+import { db } from "@/db";
+import { announcements } from "@/db/schema";
 import { formatDate } from "@/lib/utils";
-import Link from "next/link";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import React from "react";
-
-const dummyData = {
-  title: "Upcoming Seminar on AI Ethics",
-  info: "Join us for an insightful seminar on AI Ethics by Dr. John Doe on March 20, 2025, at 10 AM in Room 101.",
-  author: "Department of Computer Science",
-  date: new Date("2025-3-20"),
-  category: "Seminar",
-  attachment: "/Announcement.pdf",
-};
 
 const Page = async ({
   params,
@@ -24,25 +17,34 @@ const Page = async ({
   if (!session) {
     redirect("/auth/signin");
   }
-  const { announcementId } = await params;
+  const announcementId = Number.parseInt((await params).announcementId);
+
+  const selectedAnnouncements = await db
+    .select()
+    .from(announcements)
+    .where(eq(announcements.id, announcementId));
+
+  const announcement = selectedAnnouncements[0];
+
   return (
     <div className="pagemargin mt-4">
       <div className="flex space-x-10 items-center">
-        <h1 className="teko-heading-big">{dummyData.title}</h1>
-        <Badge className="bg-blue-dark">{dummyData.category}</Badge>
+        <h1 className="teko-heading-big">{announcement.title}</h1>
+        <Badge className="bg-blue-dark">
+          {announcement.isImportant && <div>Important</div>}
+        </Badge>
       </div>
       <h2 className="text-xl md:text-2xl font-medium font-barlow text-blue-dark mb-8">
-        {formatDate(dummyData.date)}
-      </h2>
-      <h2 className="text-lg md:text-xl font-barlow text-blue-medium mb-4">
-        - {dummyData.author}
+        {formatDate(announcement.publish)}
       </h2>
       <div className="h-[1px] bg-blue-dark mb-10"></div>
-      <p className="font-barlow text-base mb-6">{dummyData.info}</p>
+      <p className="font-barlow text-base mb-6">{announcement.info}</p>
       <div className="flex">
-        <a href={dummyData.attachment}>
-          <Button text="View details" />
-        </a>
+        {announcement.attachment && (
+          <a href={announcement.attachment}>
+            <Button text="View details" />
+          </a>
+        )}
       </div>
     </div>
   );
