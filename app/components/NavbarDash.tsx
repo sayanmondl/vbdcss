375df2a81552ebf0abd4f4720b9ec83b3b8a435b
sayan <sayanmondal5869@gmail.com>
2025-06-18 10:37:03 +0530

    Remove temporary `loggedIn` variable

--- app/components/NavbarDash.tsx
+++ app/components/NavbarDash.tsx
@@ -1,34 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { redirect } from "next/navigation";
 import { ChevronLeft } from "lucide-react";
 
 const NavbarDash = () => {
-  let loggedIn = true;
-
-  if (!loggedIn) {
-    redirect("/login");
-  } else {
-    return (
-      <nav>
-        <div className="bg-blue-fade h-20 flex items-center px-5 md:px-20">
-          <Link href="/" className="items-center gap-4">
-            <Image src="/logo2.svg" alt="vblogo" width={70} height={50} />
-          </Link>
-          <div className="flex-1"></div>
-          <Link href="/">
-            <div>
-              <button className="button-white flex items-center gap-4">
-                <ChevronLeft className=" size-4 sm:size-[18px]" />
-                <p className="mb-0.5 text-sm sm:text-base">Home</p>
-              </button>
-            </div>
-          </Link>
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav>
+      <div className="bg-blue-fade h-20 flex items-center px-5 md:px-20">
+        <Link href="/" className="items-center gap-4">
+          <Image src="/logo2.svg" alt="vblogo" width={70} height={50} />
+        </Link>
+        <div className="flex-1"></div>
+        <Link href="/">
+          <div>
+            <button className="button-white flex items-center gap-4">
+              <ChevronLeft className=" size-4 sm:size-[18px]" />
+              <p className="mb-0.5 text-sm sm:text-base">Home</p>
+            </button>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarDash;
