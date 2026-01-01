8515bd78185a9ab96875d332e341a07b649c7719
sayan <sayanmondal5869@gmail.com>
2026-01-01 11:58:54 +0530

    Fix UI bug

--- app/components/Load3LatestEvents.tsx
+++ app/components/Load3LatestEvents.tsx
@@ -5,26 +5,28 @@ import { getLatestEvents } from "@/lib/event";
 
 const Load3LatestEvents = async () => {
   const events = await getLatestEvents();
+
   return (
-    <div className="pagemargin mt-20 items-center" id="events">
-      <div className="flex items-center">
-        <a href="/#events">
-          <div className="flex w-full items-center gap-2">
-            <div className="w-4 h-12 bg-blue-dark"></div>
-            <h1 className="font-teko text-3xl font-medium text-nowrap mt-2">
-              LATEST EVENTS
-            </h1>
-          </div>
-        </a>
-        <div className="flex-1"></div>
-        <a href="/events">
-          <Button text="View All Events" />
-        </a>
-      </div>
-      <div className="my-16 grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <div className="pagemargin mt-14 md:mt-20" id="events">
+      <a href="/#events">
+        <div className="flex items-center gap-2">
+          <div className="h-10 w-1 sm:h-12 bg-blue-dark" />
+          <h1 className="font-teko text-2xl sm:text-3xl font-medium mt-1">
+            LATEST EVENTS
+          </h1>
+        </div>
+      </a>
+
+      <div className="mt-10 md:mt-16 grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
+      </div>
+
+      <div className="mt-10 flex justify-center">
+        <a href="/events">
+          <Button text="View All Events" />
+        </a>
       </div>
     </div>
   );
--- app/components/Load3LatestTournamnets.tsx
+++ app/components/Load3LatestTournamnets.tsx
@@ -5,26 +5,28 @@ import { getLatestTournaments } from "@/lib/tournament";
 
 const Load3LatestTournaments = async () => {
   const tournaments = await getLatestTournaments();
+
   return (
-    <div className="pagemargin mt-20 items-center" id="tournaments">
-      <div className="flex items-center">
-        <a href="/#tournaments">
-          <div className="flex w-full items-center gap-2">
-            <div className="w-4 h-12 bg-blue-dark"></div>
-            <h1 className="font-teko text-3xl font-medium text-nowrap mt-2">
-              SPORTS & ATHLETICS
-            </h1>
-          </div>
-        </a>
-        <div className="flex-1"></div>
-        <a href="/sports">
-          <Button text="View All Tournaments" />
-        </a>
-      </div>
-      <div className="my-16 grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <div className="pagemargin mt-14 md:mt-20" id="tournaments">
+      <a href="/#tournaments">
+        <div className="flex items-center gap-2">
+          <div className="h-10 w-1 sm:h-12 bg-blue-dark" />
+          <h1 className="font-teko text-2xl sm:text-3xl font-medium mt-1">
+            SPORTS & ATHLETICS
+          </h1>
+        </div>
+      </a>
+
+      <div className="mt-10 md:mt-16 grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
         {tournaments.map((tournament) => (
           <TournamentCard key={tournament.id} tournament={tournament} />
         ))}
+      </div>
+
+      <div className="mt-10 flex justify-center">
+        <a href="/sports">
+          <Button text="View All Tournaments" />
+        </a>
       </div>
     </div>
   );
